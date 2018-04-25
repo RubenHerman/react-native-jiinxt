d48ac72daae3869d57403b7c3f64d0e4ae492c9a
Joey Stout <joeystout@Joeys-MacBook-Pro.local>
2018-04-24 22:40:05 -0400

    made installer a little smarter

--- jiinxt-installer.sh
+++ jiinxt-installer.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
-sudo mkdir /var/opt
-sudo mv jiinxt /var/opt/jiinxt
+# Check if /var/opt exists. If not create it.
+if [ ! -d "/var/opt" ]; then
+  sudo mkdir /var/opt
+fi
 
+# Check if /var/opt/jiinxt exists. If it does, delete it.
+if [ -d "/var/opt/jiinxt" ]; then
+  sudo rm -rf /var/opt/jiinxt
+fi
+
+# Check if /usr/local/bin/jiinxt exists. If it does, delete it.
+if [ -f /usr/local/bin/jiinxt ]; then
+    sudo rm /usr/local/bin/jiinxt
+fi
+
+# Move all files and set permissions.
+sudo mv jiinxt /var/opt/jiinxt
 sudo mv runner.sh /usr/local/bin/jiinxt
 sudo chmod +x /usr/local/bin/jiinxt
